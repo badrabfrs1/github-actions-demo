a21e0b1fb239a01000e286244c9e23dab942d51b
ABOUFIRASS BADR <badr.aboufirass@segulagrp.com>
2025-02-19 15:00:16 +0100

    update

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")  # Assure-toi que Flask sait où chercher les templates
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Récupère le port de Railway
+    app.run(host="0.0.0.0", port=port)
