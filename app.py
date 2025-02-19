e877ad0f61c61c09819d234f49aea5190c91b758
ABOUFIRASS BADR <badr.aboufirass@segulagrp.com>
2025-02-19 14:29:28 +0100

    ajout page index

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Railway! 🚀"
-
-@app.route("/add", methods=["GET"])
-def add_numbers():
-    a = request.args.get("a", type=int)
-    b = request.args.get("b", type=int)
-    if a is None or b is None:
-        return jsonify({"error": "Missing parameters a or b"}), 400
-    return jsonify({"result": a + b})
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Utilise Railway PORT
-    app.run(host="0.0.0.0", port=port)
+    return render_template("index.html")
